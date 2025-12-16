5fabab4bf99bd29459ed14cf53ba2ab30b4923df
Viviana Dueñas Chavez <vivianad@microsoft.com>
2025-12-16 09:56:21 -0800

    temp

--- src/libraries/System.IO.StreamExtensions/ref/System.IO.StreamExtensions.cs
+++ src/libraries/System.IO.StreamExtensions/ref/System.IO.StreamExtensions.cs
@@ -6,6 +6,47 @@
 
 namespace System.IO.StreamExtensions
 {
+    public partial class MemoryTStream : System.IO.Stream
+    {
+        public MemoryTStream(System.Memory<byte> buffer) { }
+        public MemoryTStream(System.Memory<byte> buffer, bool writable) { }
+        public MemoryTStream(System.Memory<byte> buffer, bool writable, bool publiclyVisible) { }
+        public MemoryTStream(System.Memory<byte> buffer, int length, bool writable, bool publiclyVisible) { }
+        public override bool CanRead { get { throw null; } }
+        public override bool CanSeek { get { throw null; } }
+        public override bool CanWrite { get { throw null; } }
+        public override long Length { get { throw null; } }
+        public override long Position { get { throw null; } set { } }
+        protected override void Dispose(bool disposing) { }
+        public override void Flush() { }
+        public override System.Threading.Tasks.Task FlushAsync(System.Threading.CancellationToken cancellationToken) { throw null; }
+        public override int Read(byte[] buffer, int offset, int count) { throw null; }
+        public override int ReadByte() { throw null; }
+        public override long Seek(long offset, System.IO.SeekOrigin origin) { throw null; }
+        public override void SetLength(long value) { }
+        public bool TryGetBuffer(out System.Memory<byte> buffer) { throw null; }
+        public override void Write(byte[] buffer, int offset, int count) { }
+        public override void WriteByte(byte value) { }
+    }
+    public partial class ReadOnlyMemoryStream : System.IO.Stream
+    {
+        public ReadOnlyMemoryStream(System.ReadOnlyMemory<byte> buffer) { }
+        public ReadOnlyMemoryStream(System.ReadOnlyMemory<byte> buffer, bool publiclyVisible) { }
+        public override bool CanRead { get { throw null; } }
+        public override bool CanSeek { get { throw null; } }
+        public override bool CanWrite { get { throw null; } }
+        public override long Length { get { throw null; } }
+        public override long Position { get { throw null; } set { } }
+        protected override void Dispose(bool disposing) { }
+        public override void Flush() { }
+        public override int Read(byte[] buffer, int offset, int count) { throw null; }
+        public override int ReadByte() { throw null; }
+        public override long Seek(long offset, System.IO.SeekOrigin origin) { throw null; }
+        public override void SetLength(long value) { }
+        public bool TryGetBuffer(out System.ReadOnlyMemory<byte> buffer) { throw null; }
+        public override void Write(byte[] buffer, int offset, int count) { }
+        public override void WriteByte(byte value) { }
+    }
     public sealed partial class ReadOnlySequenceStream : System.IO.Stream
     {
         public ReadOnlySequenceStream(System.Buffers.ReadOnlySequence<byte> sequence) { }
--- src/libraries/System.IO.StreamExtensions/src/System/IO/StreamExtensions/StreamFactory.cs
+++ src/libraries/System.IO.StreamExtensions/src/System/IO/StreamExtensions/StreamFactory.cs
@@ -8,22 +8,6 @@ public static class StreamFactory
 {
     public static Stream StreamFromString(string text, Encoding? encoding = null) => new StringStream(text, encoding ?? Encoding.UTF8);
     public static Stream StreamFromReadOnlySequence(ReadOnlySequence<byte> sequence) => new ReadOnlySequenceStream(sequence);
-
-    public static Stream StreamFromStringCopy(string text, Encoding? encoding = null)
-    {
-        encoding ??= Encoding.UTF8;
-        return new MemoryStream(encoding.GetBytes(text));
-    }
-
-
-    public static Stream StreamFromReadOnlySequenceCopy(ReadOnlySequence<byte> sequence)
-    {
-        var ms = new MemoryStream();
-        foreach (var segment in sequence)
-        {
-            ms.Write(segment.Span);
-        }
-        ms.Position = 0;
-        return ms; // Copies all data into underlying buffer
-    }
+    public static Stream StreamFromData(Memory<byte> data) => new MemoryTStream(data);
+    public static Stream StreamFromROData(ReadOnlyMemory<byte> data) => new ReadOnlyMemoryStream(data);
 }
